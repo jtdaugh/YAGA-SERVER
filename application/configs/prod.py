e284dbe5486102b7becd54ecc7ff5efbec0d3b79
hellysmile <hellysmile@gmail.com>
2014-11-17 16:12:04 +0200

    Heroku setup.

--- application/configs/prod.py
+++ application/configs/prod.py
@@ -2,4 +2,16 @@ from .base import BaseConfig
 
 
 class Config(BaseConfig):
-    pass
+    AWS_ACCESS_KEY_ID = 'AKIAJSOKYB6HRKACSAMA'
+    AWS_SECRET_ACCESS_KEY = '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'
+    S3_BUCKET_NAME = 'hellysmile'
+    S3_USE_HTTPS = True
+    USE_S3 = True
+    USE_S3_DEBUG = False
+
+    CACHE_TYPE = 'simple'
+
+    SECRET_KEY = 'SECRET_KEY'
+    SECURITY_PASSWORD_SALT = 'SALT'
+
+    DEBUG_TB_ENABLED = False
