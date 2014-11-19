08cca7d66eff67cf0acfe27eb6d71d79b9ac15a1
hellysmile <hellysmile@gmail.com>
2014-11-19 21:19:01 +0200

    Fix furute print.

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
-from __future__ import absolute_import, division, unicode_literals
+from __future__ import (
+    absolute_import, division, unicode_literals, print_function
+)
 
 #!/usr/bin/env python
 import os
