211a134c1f59aad4d6cccad3c16fdefac84b8d92
hellysmile <hellysmile@gmail.com>
2014-11-24 19:30:37 +0200

    Disable test DB auth.

--- application/configs/test.py
+++ application/configs/test.py
@@ -4,4 +4,4 @@ from .local import Config as BaseConfig
 class Config(BaseConfig):
     TESTING = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://hell:@127.0.0.1:5432/yaga_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://:@127.0.0.1:5432/yaga_test'
