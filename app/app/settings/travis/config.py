d23098ebcb740c9db25ccf71df449e4827ccb54c
hellysmile <hellysmile@gmail.com>
2015-01-13 16:59:09 +0200

    Fix typo.

--- app/app/settings/travis/config.py
+++ app/app/settings/travis/config.py
@@ -73,7 +73,7 @@ class TravisConfiguration(
     # -------------------------------------------------------
     # cache configuration
     # -------------------------------------------------------
-    CACHES = CACHES = {
+    CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
             'LOCATION': '127.0.0.1:11211',
