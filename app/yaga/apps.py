15ad19374efe53d6835edd47f190880574ff9e62
hellysmile <hellysmile@gmail.com>
2015-04-15 15:23:24 +0300

    Codestyle fixes.

--- app/yaga/apps.py
+++ app/yaga/apps.py
@@ -8,7 +8,9 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class YagaAppConfig(AppConfig):
+class YagaAppConfig(
+    AppConfig
+):
     name = 'yaga'
     verbose_name = _('Yaga')
 
