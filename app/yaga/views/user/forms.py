c6dc97b2ddb62684782a32efc58fe54870015de3
hellysmile <hellysmile@gmail.com>
2015-04-28 17:24:24 +0300

    User form refactor.

--- app/yaga/views/user/forms.py
+++ app/yaga/views/user/forms.py
@@ -4,17 +4,11 @@ from future.builtins import (  # noqa
     oct, open, pow, range, round, str, super, zip
 )
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import ButtonHolder, Field, Fieldset, Layout, Submit
 from django import forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Field, Submit, Layout
-
-
-class Delete(
-    Submit
-):
-    field_classes = 'btn btn-danger'
 
 
 class UserForm(
@@ -28,9 +22,14 @@ class UserForm(
     def helper(self):
         helper = FormHelper()
         helper.layout = Layout(
-            'name',
-            Field('phone', readonly=False),
-            'is_active',
+            Fieldset(
+                _('Change User'),
+                Field('name'),
+                Field('phone', readonly=True),
+                Field('is_active')
+            ),
+            ButtonHolder(
+                Submit('submit', _('Save User'))
+            )
         )
-        helper.add_input(Submit('submit', _('Save User')))
         return helper
