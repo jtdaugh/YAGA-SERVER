5f63aacff839f5cc0ef653519c7b3e7eb9c62959
hellysmile <hellysmile@gmail.com>
2015-05-28 04:55:04 +0300

    Prepare migration.

--- fabfile.py
+++ fabfile.py
@@ -13,9 +13,9 @@ from fabric.operations import prompt
 
 APP_DIR = 'app'
 DYNOS = OrderedDict((
-    ('web', 1),
-    ('sqs', 1),
-    ('celery_broker', 1),
+    ('web', 0),
+    ('sqs', 0),
+    ('celery_broker', 0),
     ('celery_worker', 0),
 ))
 STOP_TIMEOUT = 35
