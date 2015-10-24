aaacfd38d8e58cfc5c6411c1bf0b01e0b75b1191
jtdaugh <jtdaugh@umich.edu>
2015-10-24 14:53:30 -0400

    delete groups migration

---
+++ app/yaga/migrations/0042_delete_public_groups.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+from ..choices import ApprovalChoice
+
+def delete_public_groups(apps, schema_editor):
+    Group = apps.get_model('yaga', 'Group')
+    Member = apps.get_model('yaga', 'Member')
+
+    for groupToDelete in Group.objects.filter(private=False):
+        Member.objects.filter(group=groupToDelete).delete()
+        groupToDelete.delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('yaga', '0041_remove_member_follow.py'),
+    ]
+
+    operations = [
+        migrations.RunPython(delete_public_groups),
+    ]
