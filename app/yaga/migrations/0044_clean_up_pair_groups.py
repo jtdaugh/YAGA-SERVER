42d2d2771fa357191e9a42f44e0b84458d5df4f8
jtdaugh <jtdaugh@umich.edu>
2015-10-25 11:55:34 -0400

    tried to fix mig

--- app/yaga/migrations/0044_clean_up_pair_groups.py
+++ app/yaga/migrations/0044_clean_up_pair_groups.py
@@ -31,8 +31,13 @@ def clean_up_pair_groups(apps, schema_editor):
         uniquePairGroups += 1
         
         query = Group.objects.annotate(c=Count('members')).filter(c=2).exclude(id=group.id)
-        for member in group.members.all():
-            query = query.filter(members=member)
+        
+        memCount = 0
+        for m in group.members.all():
+            query = query.filter(members=m)
+            memCount += 1
+            if (memCount == 2):
+                break
         
         for otherGroup in query:
             deletedGroupIds.append(otherGroup.id)
