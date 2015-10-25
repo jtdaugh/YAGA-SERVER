1283569099d016b81bca84c07b99c6c3a14ba9b4
jtdaugh <jtdaugh@umich.edu>
2015-10-25 02:09:35 -0400

    import transaction

--- app/yaga/migrations/0044_clean_up_pair_groups.py
+++ app/yaga/migrations/0044_clean_up_pair_groups.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations, IntegrityError
+from django.db import models, migrations, IntegrityError, transaction
 from django.db.models import Count
 
 from ..choices import ApprovalChoice
