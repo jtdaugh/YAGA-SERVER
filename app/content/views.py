d2295565a7c3f503c60c0e3a1384731e3553d599
jtdaugh <jtdaugh@umich.edu>
2015-12-14 20:58:47 -0500

    million times faster

--- app/content/views.py
+++ app/content/views.py
@@ -11,6 +11,7 @@ from django.views.generic.base import RedirectView
 from app.utils import reverse_host
 from app.views import CacheView, UserCacheView
 from yaga.models import Post
+import uuid
 
 from .conf import settings
 
@@ -51,7 +52,18 @@ class WatchVideoView(
 
     def get_context_data(self, **kwargs):
         context = super(WatchVideoView, self).get_context_data(**kwargs)
-        for p in Post.objects.select_related('user').all():
+
+        # filter UUID based on http://svn.python.org/projects/python/branches/pep-0384/Lib/uuid.py
+
+        short_id = self.kwargs['post_short_id']
+
+        low_bound = uuid.UUID(short_id + "000000000000000000000000")
+        high_bound = uuid.UUID(short_id + "FFFFFFFFFFFFFFFFFFFFFFFF")
+
+        for p in Post.objects.select_related('user').filter(
+            pk__gte=low_bound,
+            pk__lte=high_bound
+        ):
             if str(p.id).startswith(self.kwargs['post_short_id']):
                 context['post'] = p
                 context['username'] = p.user.name
